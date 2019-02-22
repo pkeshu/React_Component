baf07eb153c1adf51b49b5d072b1afeeac18fcb4
Keshar <paudelkeshar2055@gmail.com>
2019-02-22 10:50:04 +0545

    Prosps is implemented in app

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
+class Item extends React.Component{
+    render(){
+        return(
+            <div>
+                {/* <h1>From item class</h1> */}
+                <p>Titlle: {this.props.title}</p>
+                <p>Year: {this.props.year}</p>
+
+            </div>
+
+        )
+    }
+}
+// ReactDOM.render(<Item title="Leap year" year={2020}/>, document.getElementById('propId'));
 class ReactApp extends React.Component{
     state={
         name:String,
         age:Number,
         height:String
       }
+     movies=[
+          {
+              title:"Dark Pheonix",year:2019
+          },
+          {
+            title:"Avenger End Game",year:2019
+          },
+          {
+            title:"Avengers Infinity war",year:2018
+          },
+          {
+            title:"Thor Ragnorok",year:2018
+          },
+          {
+            title:"Bumblee Bee",year:2018
+          }
+        ,
+        {
+            title:"Mirror",year:2019
+          },
+          {
+            title:"Captain Marvel",year:2018
+          }
+      ]
     render(){
         return(
             <div className="basic">
@@ -12,6 +50,14 @@ class ReactApp extends React.Component{
             <p>Name: {this.state.name}</p>
             <p>Age: {this.state.age}</p>
             <p>Height: {this.state.height}</p>
+            <p>Message from prop is: {this.props.message}</p>
+            <p>Message from prop is: {this.props.info}</p>
+            <h3>List of Movies</h3>
+            {
+                this.movies.map(movies=><Item title={movies.title}
+                    year={movies.year}/>)
+            }
+
             </div>
 
         )
@@ -24,4 +70,5 @@ class ReactApp extends React.Component{
         })
     }
 }
-ReactDOM.render(<ReactApp />, document.getElementById('reactApp'));
+// ReactDOM.render(<ReactApp />, document.getElementById('reactApp'));
+ReactDOM.render(<ReactApp info={20} message="this is from prop"/>,document.getElementById("reactApp"));
